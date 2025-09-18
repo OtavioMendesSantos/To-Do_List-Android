4ff09d02aa9364c372b6bf5344bdce52ebdfaf59
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-09-18 20:12:56 -0300

    :sparkles: feat: Main Activity and Hello world

---
+++ app/src/main/java/com/example/to_do_list_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
