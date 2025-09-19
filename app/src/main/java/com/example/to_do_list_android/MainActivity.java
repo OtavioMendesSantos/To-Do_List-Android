8d53ce59730b1f794943c6ff95ca2dfb8fc134a2
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-09-18 21:28:59 -0300

    :sparkles: feat: add a second page HelloActivity and navigation to it

--- app/src/main/java/com/example/to_do_list_android/MainActivity.java
+++ app/src/main/java/com/example/to_do_list_android/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.to_do_list_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnBoaNoite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBoaNoite = findViewById(R.id.btnBoaNoite);
+
+        btnBoaNoite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelloActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
